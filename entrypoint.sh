46ea3a730dd433770c2c72601fec48cacd0a43d7
notchwis <kodenothing@gmail.com>
2026-05-03 22:23:47 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,28 +5,26 @@ set -e
 mkdir -v -p ~/.local/share/godot/export_templates
 cp -a /root/.local/share/godot/export_templates/. ~/.local/share/godot/export_templates/
 
-
 if [ "$3" != "" ]
 then
     SubDirectoryLocation="$3/"
 fi
 
-mode="export-release"
+mode="release"
 if [ "$6" = "true" ]
 then
     echo "Exporting in debug mode!"
-    mode="export-debug"
+    mode="debug"
 fi
 
-# Export for project
+# Export for project (Godot 4 syntax)
 echo "Building $1 for $2"
 mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
 cd "$GITHUB_WORKSPACE/$5"
-godot --headless --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
+godot --headless --export-${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
 echo "Build Done"
 
 echo ::set-output name=build::build/${SubDirectoryLocation:-""}
-
 
 if [ "$4" = "true" ]
 then
